5bbcbe8ffbe8a81f8609991afc0997cfd3fdb3d2
ihar.kudla <ihar.kudla@winfinity.live>
2021-06-24 19:25:07 +0300

    Добавление прослушки событий

--- src/core/DomListener.js
+++ src/core/DomListener.js
@@ -1,3 +1,7 @@
+import { capitalize } from "@core/utils";
+
+// TODO Добавление прослушки событий 03_08
+
 export class DomListener {
   constructor($root, listeners = []) {
     if (!$root) {
@@ -8,8 +12,18 @@ export class DomListener {
   }
 
   initDOMListeners() {
-    console.log(this.listeners)
+    this.listeners.forEach(listener => {
+      const method = getMethodName(listener);
+      if (!this[method]) {
+        throw new Error(`Method ${method} is not implemented in ${this.name || ''} Component`);
+      }
+      this.$root.on(listener, this[method].bind(this));
+    })
   }
 
   removeDOMListeners() {}
+}
+
+function getMethodName(eventName) {
+  return "on" + capitalize(eventName)
 }
